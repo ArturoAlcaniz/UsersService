2c443c5f0ab68fd00bc964b2f4e55b3e9eade05c
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-03-12 18:11:39 +0000

    fixed paths

--- src/paths.ts
+++ src/paths.ts
@@ -2,9 +2,9 @@ import 'module-alias/register';
 import { addAliases } from 'module-alias';
 
 addAliases({
-    '@entities-lib/*': `${__dirname}/../../entities-lib/*`,
-    '@config-lib/*': `${__dirname}/../../config-lib/*`,
-    '@commons/*': `${__dirname}/commons/*`,
-    '@hashing/*': `${__dirname}/hashing/*`,
-    '@root/*': `${__dirname}/../`
+    '@entities-lib': `${__dirname}/../../entities-lib`,
+    '@config-lib': `${__dirname}/../../config-lib`,
+    '@commons': `${__dirname}/commons`,
+    '@hashing': `${__dirname}/hashing`,
+    '@root': `${__dirname}/..`
 });
