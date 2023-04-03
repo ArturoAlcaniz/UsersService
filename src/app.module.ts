f737f19e11e1d1e610ad21b723b85c714cd89bc0
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-04-03 21:19:51 +0000

    inject metrics provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,30 @@
-import {ConfigModule} from "@nestjs/config";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {User} from "@entities-lib/src/entities/user.entity";
-import {jwtConfig} from "@config-lib/src/jwt.config";
-import {AppController} from "./app.controller";
-import {HashingModule} from "./hashing/hashing.module";
-import {UsersModule} from "./users/users.module";
-import {Module} from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "@entities-lib/src/entities/user.entity";
+import { jwtConfig } from "@config-lib/src/jwt.config";
+import { AppController } from "./app.controller";
+import { HashingModule } from "./hashing/hashing.module";
+import { UsersModule } from "./users/users.module";
+import { Module } from "@nestjs/common";
 import path from "path";
-import {HttpModule} from "@nestjs/axios";
-import {ThrottlerModule} from "@nestjs/throttler";
-import {WinstonModule} from "nest-winston";
+import { HttpModule } from "@nestjs/axios";
+import { ThrottlerModule } from "@nestjs/throttler";
+import { WinstonModule } from "nest-winston";
 import * as winston from "winston";
-import {Product} from "@entities-lib/src/entities/product.entity";
-import {ProductImage} from "@entities-lib/src/entities/productimage.entity";
-import {Payment} from "@entities-lib/src/entities/payment.entity";
-import {Code} from "@entities-lib/src/entities/code.entity";
-import {Invoice} from "@entities-lib/src/entities/invoice.entity";
-import {InvoiceItem} from "@entities-lib/src/entities/invoiceItem.entity";
-import {JwtModule} from "@nestjs/jwt";
-import {DevtoolsModule} from "@nestjs/devtools-integration";
+import { Product } from "@entities-lib/src/entities/product.entity";
+import { ProductImage } from "@entities-lib/src/entities/productimage.entity";
+import { Payment } from "@entities-lib/src/entities/payment.entity";
+import { Code } from "@entities-lib/src/entities/code.entity";
+import { Invoice } from "@entities-lib/src/entities/invoice.entity";
+import { InvoiceItem } from "@entities-lib/src/entities/invoiceItem.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { DevtoolsModule } from "@nestjs/devtools-integration";
 import { MetricsController } from "./metrics.controller";
+import * as Prometheus from 'prom-client';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({isGlobal: true}),
+        ConfigModule.forRoot({ isGlobal: true }),
         JwtModule.register(jwtConfig()),
         TypeOrmModule.forRoot({
             type: "mariadb",
@@ -69,8 +70,13 @@ import { MetricsController } from "./metrics.controller";
             http: process.env.NODE_ENV === "develop",
         }),
     ],
-    providers: [],
+    providers: [
+        {
+            provide: 'PROMETHEUS',
+            useValue: new Prometheus.Registry(),
+        },
+    ],
     controllers: [AppController, MetricsController],
-    exports: [WinstonModule, ThrottlerModule, JwtModule],
+    exports: [WinstonModule, ThrottlerModule, JwtModule, 'PROMETHEUS'],
 })
-export class ApplicationModule {}
+export class ApplicationModule { }
--- src/metrics.controller.ts
+++ src/metrics.controller.ts
@@ -1,14 +1,13 @@
-import { Controller, Get, Response } from '@nestjs/common';
-import * as prometheus from 'prom-client';
+import { Controller, Get, Inject, Response } from '@nestjs/common';
+import * as Prometheus from 'prom-client';
 
 @Controller()
 export class MetricsController {
-    constructor(private readonly prometheus: prometheus.Registry) {}
+  constructor(@Inject('PROMETHEUS') private readonly prometheus: Prometheus.Registry) {}
 
-
-    @Get('/metrics')
-    getMetrics(@Response() res) {
-        res.set('Content-Type', this.prometheus.contentType);
-        res.end(this.prometheus.metrics());
-    }
+  @Get('/metrics')
+  getMetrics(@Response() res) {
+    res.set('Content-Type', this.prometheus.contentType);
+    res.end(this.prometheus.metrics());
+  }
 }
