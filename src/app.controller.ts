7a9171a09d627c903ad5d65231dd50474066af84
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-04-03 20:41:38 +0000

    fix metrics

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import {Controller, Get, Res} from "@nestjs/common";
 import {ApiTags} from "@nestjs/swagger";
-import * as promClient from "prom-client";
+import * as Prometheus from 'prom-client';
 import {Response} from "express";
 
 @ApiTags("APP Controller")
 @Controller()
 export class AppController {
+    @Get('metrics')
+    async getMetrics(@Res() res: Response) {
+      res.set('Content-Type', Prometheus.register.contentType);
+      res.send(await Prometheus.register.metrics());
+    }
 }
