dcee8b89c9b3f827d6ad711a2056934d8c36b492
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-04-03 20:14:38 +0000

    fix metrics

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,74 @@
 import "./paths";
-import {NestFactory} from "@nestjs/core";
-import {ApplicationModule} from "./app.module";
-import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
-import {ValidationPipe} from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ApplicationModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 import * as Prometheus from "prom-client";
 import cookieParser from "cookie-parser";
 
 async function bootstrap() {
-    const promBundle = require("express-prom-bundle");
-    // Configura el almacenamiento persistente para las métricas
-    const prometheus = new Prometheus.Registry();
-    Prometheus.collectDefaultMetrics({register: prometheus});
+  // Configura el almacenamiento persistente para las métricas
+  const prometheus = new Prometheus.Registry();
+  Prometheus.collectDefaultMetrics({ register: prometheus });
 
-    // Crea la aplicación NestJS
-    const app = await NestFactory.create(ApplicationModule, {
-        snapshot: true,
-    });
+  // Crea la aplicación NestJS
+  const app = await NestFactory.create(ApplicationModule, {
+    snapshot: true,
+  });
 
-    // Configura Swagger
-    const config = new DocumentBuilder()
-        .setTitle("API Document")
-        .setDescription("TFG")
-        .setVersion("1.0")
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup("docs", app, document);
+  // Configura Swagger
+  const config = new DocumentBuilder()
+    .setTitle("API Document")
+    .setDescription("TFG")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
-    // Configura los middleware globales
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            forbidNonWhitelisted: true,
-            forbidUnknownValues: true,
-        })
-    );
-    app.use(cookieParser());
+  // Configura los middleware globales
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    })
+  );
+  app.use(cookieParser());
 
-    // Agrega el middleware de prometheus con la opción customLabels
-    app.use(
-        promBundle({
-            includeMethod: true,
-            includePath: true,
-            promClient: {
-                register: prometheus,
-            },
-            metricsPath: "/metrics",
-            customLabels: {
-                method: (req) => req.method,
-                path: (req) => req.path,
-                status: (res) => res.statusCode.toString(),
-            },
-        })
-    );
+  // Agrega el middleware de prometheus con la opción customLabels
+  app.use((req, res, next) => {
+    const end = res.end;
+    res.end = function (...args: any) {
+      httpRequestCounter
+        .labels(req.method, req.path, res.statusCode.toString())
+        .inc();
+      end.apply(res, args);
+    };
+    next();
+  });
+  
 
-    // Elimina el middleware para registrar las solicitudes HTTP
-    // Registra la métrica http_requests_total en el middleware de prometheus
-    const httpRequestCounter = new Prometheus.Counter({
-        name: "http_requests_total",
-        help: "Total number of HTTP requests",
-        labelNames: ["method", "path", "status"],
-        registers: [prometheus],
-    });
-    app.use((req, res, next) => {
-        const end = res.end;
-        res.end = function (...args: any) {
-            httpRequestCounter
-                .labels(req.method, req.path, res.statusCode.toString())
-                .inc();
-            end.apply(res, args);
-        };
-        next();
-    });
+  // Elimina el middleware para registrar las solicitudes HTTP
+  // Registra la métrica http_requests_total en el middleware de prometheus
+  const httpRequestCounter = new Prometheus.Counter({
+    name: "http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "path", "status"],
+    registers: [prometheus],
+  });
+  app.use((req, res, next) => {
+    const end = res.end;
+    res.end = function (...args: any) {
+      httpRequestCounter
+        .labels(req.method, req.path, res.statusCode.toString())
+        .inc();
+      end.apply(res, args);
+    };
+    next();
+  });
 
-    // Inicia la aplicación
-    await app.listen(process.env.USERS_CONTAINER_PORT);
+  // Inicia la aplicación
+  await app.listen(process.env.USERS_CONTAINER_PORT);
 }
+
 bootstrap();
