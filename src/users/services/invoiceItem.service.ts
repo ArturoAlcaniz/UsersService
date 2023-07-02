669fd4af0b30e12d7c8afd5ea52cbf9a8a663250
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-07-02 16:10:37 +0000

    fix import

--- src/users/services/invoiceItem.service.ts
+++ src/users/services/invoiceItem.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {BaseService} from "@commons/service.commons";
-import {InvoiceItem} from "@entities-lib/src/entities/InvoiceItem.entity";
+import {InvoiceItem} from "@entities-lib/src/entities/invoiceItem.entity";
 
 
 @Injectable()
